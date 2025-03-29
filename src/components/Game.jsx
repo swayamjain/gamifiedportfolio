904ca51404ccc1435e09703445196d43f49b2d8f
Swayam Jain <SW4Y4MJ4IN@GMAIL.COM>
2025-03-29 23:16:14 +0530

    set debug to false

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -268,7 +268,7 @@ const Game = () => {
       physics: {
         default: "arcade",
         arcade: {
-          debug: true,
+          debug: false,
           gravity: { y: 0 }
         },
       },
